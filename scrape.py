be7e0b36c6e069bad295198ce42d82d09c6bfd2b
Cameron Chambers <cameronchambers@trentu.ca>
2019-07-22 14:35:05 -0400

    Created a demo program of scraping webpages in python

---
+++ scrape.py
@@ -0,0 +1,24 @@
+# An example of scraping web pages with python using the BeautifulSoup library
+
+# Used for handling program arguments
+import sys
+
+# Used for making HTTP requests
+import requests
+
+# Used for scraping web pages
+from bs4 import BeautifulSoup
+
+# Set the URL you want to webscrape from
+url = 'https://en.wikipedia.org/wiki/Monkey'
+
+# Connect to the URL
+response = requests.get(url)
+
+# # Parse HTML and save to BeautifulSoup object
+soup = BeautifulSoup(response.text, "html.parser")
+
+#Scrape content div
+contentDiv = soup.find('div', {'id': 'bodyContent'}).findAll('p')
+
+print(contentDiv[1].text)
